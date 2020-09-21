7b435849ae8350865beb5aed93c97c96a80a2fbf
1704371 <basupuhabi@yahoo.com>
2020-09-21 09:49:43 +0530

    Updates

---
+++ precache-manifest.f8e06a042f07b65d7c3ed10f9ef70675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0901774c28c7b0560bbb44e673ed92",
+    "url": "/%20//puhabibasu.github.io/skill-swap/index.html"
+  },
+  {
+    "revision": "b8b530d798171e234e4a",
+    "url": "/%20//puhabibasu.github.io/skill-swap/static/css/main.bd11ab3b.chunk.css"
+  },
+  {
+    "revision": "e3809e96670b7358ac62",
+    "url": "/%20//puhabibasu.github.io/skill-swap/static/js/2.9291eb3b.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/%20//puhabibasu.github.io/skill-swap/static/js/2.9291eb3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b530d798171e234e4a",
+    "url": "/%20//puhabibasu.github.io/skill-swap/static/js/main.ff1c526e.chunk.js"
+  },
+  {
+    "revision": "c8936ba3e2537c7beed6",
+    "url": "/%20//puhabibasu.github.io/skill-swap/static/js/runtime-main.6d147498.js"
+  }
+]);
